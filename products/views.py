68ac44e4df11c85b443f4f712e00c65a59dd552f
lusukama <lusukama.s@gmail.com>
2026-05-23 18:14:52 +0200

    Implemented modern ecommerce frontend UI with Bootstrap

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all().order_by('-created_at')
+    return render(request, 'products/index.html', {'products': products})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'products/detail.html', {'product': product})
